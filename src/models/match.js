8786cb878a6dcaba5dd8f76b479d052062c00a73
bijay_d <iambijaydulal@gmail.com>
2023-12-18 20:37:03 +0545

    Added game duration migration

--- src/models/match.js
+++ src/models/match.js
@@ -31,6 +31,11 @@ module.exports = (sequelize, DataTypes) => {
 				type: DataTypes.STRING,
 				allowNull: false,
 			},
+			duration: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				defaultValue: 60,
+			},
 			isSearching: {
 				type: DataTypes.BOOLEAN,
 				defaultValue: false,
